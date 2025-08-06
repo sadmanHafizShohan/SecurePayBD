4388496828c6ada183ceae253e7d08649f7d3411
snrshohan <snrshohan21@gmail.com>
2025-08-06 16:54:27 +0600

    login page authentication successful

--- js/login.js
+++ js/login.js
@@ -1,10 +1,15 @@
 document.getElementById("login-button").addEventListener('click', function(e){
     e.preventDefault();
     const pinNumber = document.getElementById("pin-number").value;
+    const mobileNumber = document.getElementById("mobile-number").value;
 
-    if(pinNumber === "123456"){
-        window.location = "/dashboard.html"
-    }else{
-        alert("wrong pin! the pin number is 123456")
-    }
+    if( pinNumber !== "123456" && mobileNumber.length !== 11){
+        alert("wrong pin and mobile number! try again...")
+    }else if( pinNumber !== "123456"){
+        alert("wrong pin! by default the pin is 123456")
+    }else if( mobileNumber.length !== 11){
+        alert("Your mobile number should be 11 digit")
+    }else 
+    window.location = "/dashboard.html"
+
 })
